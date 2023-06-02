5ef961ec3ac18b62dff6e873dad8b3be2953a1e9
artgolden <artemiy.golden@protonmail.com>
2023-06-02 12:29:08 +0200

    Added workaround for shutil move to samba share errors. Refactored main compression function.

--- compress_tiffs.py
+++ compress_tiffs.py
@@ -6,7 +6,47 @@ from tqdm import tqdm
 from gooey import Gooey
 
 
-def compress_tiff_files(input_path, quality, compression, threads, cache_dir, replace_files):
+def compress_one_file(file_path, pbar, quality, compression, threads, cache_dir, replace_files):
+    temp_file_path = file_path + '.part'
+    temp_file_path_beside_original = temp_file_path
+    try:
+        if cache_dir:
+            cache_file_path = os.path.join(cache_dir, os.path.basename(temp_file_path))
+            temp_file_path = cache_file_path
+
+        # Compress the TIFF file using the specified algorithm and quality
+        with tifffile.TiffWriter(temp_file_path) as tiff:
+            tiff.write(tifffile.imread(file_path), compression=(compression, quality), maxworkers=threads)
+
+        if cache_dir:
+            try:
+                # Move the compressed file from the temporary cache directory to the final destination
+                shutil.move(temp_file_path, temp_file_path_beside_original)
+            except OSError as e:
+                # That is a workaround when shutil is raising an error when copying file to samba share where you can't copy permissions
+                if e.errno == 95:
+                    if os.path.isfile(temp_file_path):
+                        os.remove(temp_file_path)
+                else:
+                    print(f"Error compressing: {file_path}\n" + str(e))
+            pbar.update(1)
+            print(f"Compressed: {file_path}")
+        if replace_files:
+            # Replace the original file with the compressed file
+            try:
+                shutil.move(temp_file_path_beside_original, file_path)
+            except OSError as e:
+                if e.errno == 95:
+                    pass
+                else:
+                    print(f"Error compressing: {file_path}\n" + str(e))
+            pbar.update(1)
+            print(f"Compressed: {file_path}")
+    except Exception as e:
+        print(f"Error compressing: {file_path}")
+        print(e)
+
+def compress_tiff_files(input_path, *args):
     """
     Compresses the TIFF files in the given input path (folder or file) using the specified compression algorithm and quality percentage.
     Creates a new compressed TIFF file with the name '.part' and replaces the original file(s).
@@ -21,55 +61,12 @@ def compress_tiff_files(input_path, quality, compression, threads, cache_dir, re
 
         with tqdm(total=len(tiff_files), ncols=80, desc="Progress") as pbar:
             for file_path in tiff_files:
-                temp_file_path = file_path + '.part'
-                temp_file_path_beside_original = temp_file_path
-                try:
-                    if cache_dir:
-                        cache_file_path = os.path.join(cache_dir, os.path.basename(temp_file_path))
-                        temp_file_path = cache_file_path
-
-                    # Compress the TIFF file using the specified algorithm and quality
-                    with tifffile.TiffWriter(temp_file_path) as tiff:
-                        tiff.write(tifffile.imread(file_path), compression=(compression, quality), maxworkers=threads)
-
-                    if cache_dir:
-                        # Move the compressed file from the temporary cache directory to the final destination
-                        shutil.move(temp_file_path, temp_file_path_beside_original)
-                    if replace_files:
-                        # Replace the original file with the compressed file
-                        shutil.move(temp_file_path_beside_original, file_path)
-                    pbar.update(1)
-                    print(f"Compressed: {file_path}")
-                except Exception as e:
-                    print(f"Error compressing: {file_path}")
-                    print(e)
+                compress_one_file(file_path, pbar, *args)
     else:
         # Input path is a file
         file_path = input_path
-        temp_file_path = file_path + '.part'
-        temp_file_path_beside_original = temp_file_path
-
-        try:
-            if cache_dir:
-                cache_file_path = os.path.join(cache_dir, os.path.basename(temp_file_path))
-                temp_file_path = cache_file_path
-            # Compress the TIFF file using the specified algorithm and quality
-            with tifffile.TiffWriter(temp_file_path) as tiff:
-                tiff.write(tifffile.imread(file_path), compression=(compression, quality), maxworkers=threads)
-
-            if cache_dir:
-                # Move the compressed file from the temporary cache directory to the final destination
-                shutil.move(temp_file_path, temp_file_path_beside_original)
-
-            if replace_files:
-                # Replace the original file with the compressed file
-                shutil.move(temp_file_path, file_path)
-            print(f"Compressed: {file_path}")
-        except Exception as e:
-            print(f"Error compressing: {file_path}")
-            print(e)
-
-
+        with tqdm(total=1, ncols=80, desc="Progress") as pbar:
+            compress_one_file(file_path, pbar, *args)
 @Gooey
 def main():
     parser = argparse.ArgumentParser(description="Compress TIFF files using different compression algorithms.")
